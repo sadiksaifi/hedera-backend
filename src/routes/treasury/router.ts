d93c948677d750d709b2731e846d33582b099ab6
Sadik Saifi <email@sadiksaifi.dev>
2024-06-06 16:33:14 +0530

    fix: tresury route for permance

--- src/routes/treasury/router.ts
+++ src/routes/treasury/router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { validateRequest } from "zod-express-middleware";
 import { STreasuryData } from "@/schemas/coin";
-import { getCoinInfo, getTreasury, unfreezeStableCoin } from "@/lib/hedera";
+import { getCoinInfo, getTreasury } from "@/lib/hedera";
 
 export const router: ExpressRouter = async () => {
   const router = Router();
@@ -13,26 +13,20 @@ export const router: ExpressRouter = async () => {
       const query = req.query;
       const tokenList = await getTreasury(query);
 
-      const data: {
-        name: string;
-        symbol: string;
-        tokenId: string;
-        myBalance: number;
-        isDeleted: boolean;
-      }[] = [];
-
-      for (let i = 0; i < 5; i++) {
-        const { tokenId, ...token } = await getCoinInfo({
-          tokenId: tokenList[i].token_id,
-        });
-        data.push({
-          tokenId: tokenId.toString(),
-          myBalance: tokenList[i].balance,
-          name: token.name,
-          symbol: token.symbol,
-          isDeleted: token.isDeleted,
-        });
-      }
+      const data = await Promise.all(
+        tokenList.map(async (token) => {
+          const { tokenId, ...tokenInfo } = await getCoinInfo({
+            tokenId: token.token_id,
+          });
+          return {
+            tokenId: tokenId.toString(),
+            myBalance: token.balance,
+            name: tokenInfo.name,
+            symbol: tokenInfo.symbol,
+            isDeleted: tokenInfo.isDeleted,
+          };
+        })
+      );
 
       res.json({ data });
     }
