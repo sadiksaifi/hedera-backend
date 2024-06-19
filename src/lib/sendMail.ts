3e7a6ea8da2c76dbed6a4ee93b2d1a060c155bb7
Aryan Thakur <aryan197297@gmail.com>
2024-06-19 22:03:08 +0530

    feat: add the sendgrid function

--- src/lib/sendMail.ts
+++ src/lib/sendMail.ts
@@ -1,12 +1,7 @@
 import { Resend } from "resend";
-
 // import sgMail from "@sendgrid/mail";
+
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-// const SEND_GRID_FROM_NAME = process.env.SEND_GRID_FROM_NAME!;
-// const SEND_GRID_FROM_EMAIL = process.env.SEND_GRID_FROM_EMAIL!;
-// const SEND_GRID_API = process.env.SEND_GRID_API!;
-
 export const sendMail = async (email: string, body: string) => {
   const { data, error } = await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
@@ -17,16 +12,24 @@ export const sendMail = async (email: string, body: string) => {
 
   if (error) throw new Error(error.message);
 
-  // const msg: sgMail.MailDataRequired = {
-  //   from: { name: SEND_GRID_FROM_NAME, email: SEND_GRID_FROM_EMAIL },
-  //   to: [email],
-  //   subject: "Verify you Account",
-  //   html: body,
-  // };
-  // sgMail.setApiKey(SEND_GRID_API!);
-  //
-  // const res = await sgMail.send(msg);
-  // console.log(res);
-
   return data;
 };
+
+// const SEND_GRID_FROM_NAME = process.env.SEND_GRID_FROM_NAME!;
+// const SEND_GRID_FROM_EMAIL = process.env.SEND_GRID_FROM_EMAIL!;
+// const SEND_GRID_API = process.env.SEND_GRID_API!;
+//
+// export const sendMail = async (email: string, body: string) => {
+//   const msg: sgMail.MailDataRequired = {
+//     from: { name: SEND_GRID_FROM_NAME, email: SEND_GRID_FROM_EMAIL },
+//     to: [email],
+//     subject: "Verify you Account",
+//     html: body,
+//   };
+//   sgMail.setApiKey(SEND_GRID_API!);
+//
+//   const res = await sgMail.send(msg);
+//   console.log(res);
+//
+//   return data;
+// };
